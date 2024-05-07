a0b874a82db7043916685bde82e827e834155e98
kamoasko <kamdesigning@gmail.com>
2024-05-07 18:00:56 +0400

    homepage form changes, custom select added to it

--- assets/js/general.js
+++ assets/js/general.js
@@ -137,3 +137,31 @@ mobLangChange.forEach((option) => {
     }
   });
 });
+
+// form selects open
+
+const responseSelects = document.querySelectorAll(".rff");
+
+responseSelects.forEach((select) => {
+  select.addEventListener("click", (e) => {
+    // Close any previously opened options
+    responseSelects.forEach((s) => {
+      if (s !== e.currentTarget && s.classList.contains("active")) {
+        s.classList.remove("active");
+      }
+    });
+
+    // Toggle the "active" class for the clicked option
+    e.currentTarget.classList.toggle("active");
+  });
+
+  const options = select.querySelector(".response__form-options");
+  const selectedOptionText = select.querySelector(".selected > span");
+
+  options.addEventListener("click", (e) => {
+    if (e.target.classList.contains("flex-center")) {
+      const optionText = e.target.textContent.trim();
+      selectedOptionText.textContent = optionText;
+    }
+  });
+});
